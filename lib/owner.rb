f50822797b36003dadd8ccf2164cf5ce7eec36f0
Andy Maskiell <maskiella@gmail.com>
2018-01-04 01:35:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,8 +32,14 @@ class Owner
     @pets[:fishes] << Fish.new(fish_name)
   end
 
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
   def buy_dog(dog_name)
     @pets[:dogs] << Dog.new(dog_name)
   end
+
+  
 
 end
